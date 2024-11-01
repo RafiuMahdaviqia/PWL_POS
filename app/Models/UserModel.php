aea84120a56aedf2efbc3ba905cd2fee9a04f359
Rafi'u Mahdaviqia <rafi'u@polinema.ac.id>
2024-11-01 18:19:49 +0700

    JS 4 - Praktikum 2.4

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -1,84 +1,22 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Models;
 
-use Illuminate\Http\Request;
-use App\Models\UserModel;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class UserController extends Controller
+class UserModel extends Model
 {
-    public function index(){
-        // JOBSHEET 3   
-        // tambah data
-        // $data=[
-        //     'username' => 'customer 1',
-        //     'nama' => 'pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 5
-        // ];
-        // UserModel::insert($data);
+    use HasFactory;
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
 
-        //tambah data dengan Eloquent
-        // $data = [
-        //     'nama' => 'Pelanggan pertama',
-        // ];
-        // UserModel::where('username', 'customer 1')->update($data); //update data user
+    // JOBSHEET 4
+    // prak 1
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    // protected $fillable = ['level_id', 'username', 'nama'];
 
-        // // coba akses model UserModel
-        // $user = UserModel::all(); //ambil semua data m_user
-        // return view('user', ['data' => $user]);
-
-        // JOBSHEET 4
-        // no 2
-        // $data = [
-        //     'level_id' => 2,
-        //     'username' => 'manager_dua',
-        //     'nama' => 'Manager 2',
-        //     'password' => Hash::make('12345')   
-        // ];
-        // UserModel::create($data);
-        // $user = UserModel::all();
-        // return view('user', ['data' => $user]);
-
-        // no 5
-        // $data = [
-        //     'level_id' => 2,
-        //     'username' => 'manager_tiga',
-        //     'nama' => 'Manager 3',
-        //     'password' => Hash::make('12345')
-        // ];
-        // UserModel::create($data);
-        // $user = UserModel::all();
-        // return view('user', ['data' => $user]);
-
-        //Prak 2.1
-        // no 1
-        // $user = UserModel::find(1);
-        // return view('user', ['data' => $user]);
-        // no 4
-        // $user = UserModel::where('level_id', 1)->first();
-        // return view('user', ['data' => $user]);
-        // no 6
-        // $user = UserModel::firstWhere('level_id', 1);
-        // return view('user', ['data' => $user]);
-        // no 8
-        // $user = UserModel::findOr(1, ['username', 'nama'], function(){
-        //     abort(404);
-        // });
-        // return view('user', ['data' => $user]);
-        // no 10
-        // $user = UserModel::findOr(20, ['username', 'nama'], function(){
-        //     abort(404);
-        // });
-        // return view('user', ['data' => $user]);
-
-        // prak 2.2
-        // no 2
-        // $user = UserModel::findOrFail(1);
-        // return view('user', ['data' => $user]);
-
-        // no 3
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
-    }
+    // prak 2.4
+    // protected $table = 'user';
+    
 }
